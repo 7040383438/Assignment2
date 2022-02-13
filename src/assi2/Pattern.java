e1914829eea9b58bab2bd594928b77efdc9600e8
unknown <priyataware7@gmail.com0>
2022-02-13 19:25:25 +0530

    Initial commit

---
+++ src/assi2/Pattern.java
@@ -0,0 +1,19 @@
+package assi2;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	for(int i=5;i>=1;i--)
+		{
+			for(int j=i;j>0;j--)
+			{
+				System.out.print(j+" ");
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ src/assi2/Question6.java
@@ -0,0 +1,20 @@
+package assi2;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 for (int i = 5; i >= 1; i--) 
+	        {
+	            for (int j = 1; j <= i; j++)
+	            {
+	                System.out.print(j+" ");
+	            }
+	             
+	            System.out.println();
+	        }
+
+	}
+
+}
